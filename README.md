2d8d9329a9d94921fa0d9fb4a7e8c176dc5bb30d
Vitor Forbrig <vitorforbrig@gmail.com>
2020-04-08 21:21:29 -0300

    get profile, profile display, readme

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # pyrepo
+
 Python repository previwer (to learn python + flask)
+
+![pyrepo_1](https://raw.githubusercontent.com/Forbrig/survetric/master/pyrepo_1.png)
+![pyrepo_2](https://raw.githubusercontent.com/Forbrig/survetric/master/pyrepo_2.png)
+![pyrepo_3](https://raw.githubusercontent.com/Forbrig/survetric/master/pyrepo_3.png)
+![pyrepo_4](https://raw.githubusercontent.com/Forbrig/survetric/master/pyrepo_4.png)
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+
+### Prerequisites
+
+* To run will need to have **python3** (_or equivalent)) on your machine.
+* _flask_;
+* _requests_;
+
+To install the requirements go to _src_ and type on terminal:
+
+```bash
+    pip install -r requirements.txt
+```
+
+### Running
+
+Set the flask variables to run the server.py on development mode:
+
+```bash
+    export FLASK_APP=server.py
+```
+
+```bash
+    export FLASK_ENV=development
+```
+
+Then run the application on _src_ folder:
+
+```bash
+    flask run
+```
+
+## Authors
+
+* **Vitor G. Forbrig** - [Forbrig](https://github.com/Forbrig)
--- src/server.py
+++ src/server.py
@@ -1,34 +1,73 @@
 from flask import Flask, render_template, abort, jsonify, request, redirect, url_for
-import json, requests
+import json, requests, datetime
+# import base64
 
 from model import db, save_db
 
 app = Flask(__name__)
 
-github_name = "Forbrig"
+github_name = ""
 repos = []
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def home():
-    return render_template('home.html')
+    global github_name
+    message = ""
+    if request.method == "POST":
+        github_name = request.form['username']
+        user_data = json.loads(requests.get('https://api.github.com/users/'+github_name).content.decode('utf-8'))
+        if "message" in user_data:
+            message = user_data["message"]
+            user_data = None
+    else:
+        user_data = json.loads(requests.get('https://api.github.com/users/'+github_name).content.decode('utf-8'))
+        if "message" in user_data:
+            message = user_data["message"]
+            user_data = None
+
+    return render_template('home.html', user = user_data, message = message)
 
 @app.route('/repository')
 def repositories():
     # repo = db
-    repos = json.loads(requests.get('https://api.github.com/users/'+github_name+'/repos').content.decode('utf-8'))
-    return render_template('repositories.html', author = github_name, repos = repos)
+    global repos
+    global github_name
 
-@app.route('/repository/<string:index>')
+    if github_name != "":
+        repos = json.loads(requests.get('https://api.github.com/users/'+github_name+'/repos').content.decode('utf-8'))
+        if "message" in repos:
+            return redirect(url_for("home", user = None))
+        else:
+            return render_template('repositories.html', author = github_name, repos = repos, total = str(len(repos)))
+    else:
+        return redirect(url_for("home", user = None))
+
+
+@app.route('/repository/<int:index>')
 def repository(index):
     try:
-        # repo = db[index]
-        # return render_template('repository.html', repo = repo, index = index, max_index = len(db) - 1)
+        global repos
+        repo = repos[index]
+        # return render_template('repository.html', repo = repo, index = index, max_index = len(repos) - 1)
 
-        repo = json.loads(requests.get('https://api.github.com/repos/'+github_name+'/'+index).content.decode('utf-8'))
-        print(repo)
-        return render_template('repository.html', repo = repo, index = 0, max_index = 123456)
+        # repo = json.loads(requests.get('https://api.github.com/repos/'+github_name+'/'+index).content.decode('utf-8'))
+        
+        # repo["created_at"] = datetime.datetime.strptime(repo["created_at"].replace("T", " ").replace("Z", ""), '%Y-%m-%d %H:%M:%S')
+        # readme = json.loads(requests.get('https://api.github.com/repos/'+github_name+'/'+index+'/readme').content.decode('utf-8'))
+        # print(readme)
+ 
+        return render_template(
+            'repository.html', repo = repo, index = index, max_index = len(repos) - 1)
     except IndexError:
         abort(404)
+
+@app.route('/remove_user')
+def remove_user():
+    global github_name
+    global repos
+    github_name = ""
+    repos = []
+    return redirect(url_for("home", user = None))
 
 @app.route('/add_repository', methods=["GET", "POST"])
 def add_repository():
@@ -42,10 +81,6 @@ def add_repository():
         return redirect(url_for("repository", index = len(db) - 1))
     else:
         return render_template('add_repository.html')
-
-# @app.route('/github')
-# def get_github():
-#     return requests.get('https://api.github.com/users/'+github_name+'/repos').content
 
 ################## SERVING AS API ####################
 
